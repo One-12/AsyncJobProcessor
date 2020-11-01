c61968212ce686bd367393076afde5329c7b6f1e
Prakash <prakash.selvaraj@outlook.com>
2020-11-01 15:30:45 +0530

    -- tag trending calculated on tagProcess

--- tag/tags.processor.js
+++ tag/tags.processor.js
@@ -1,18 +1,21 @@
 const tag = require("./tag.entity");
+const tagTrend = require('./tag.trending.inc');
 
 exports.tagProcess = async job => {
   const data = job.data;
-  if (!data) {
+  if (!data || !data.tags || !data.userId) {
     return;
   }
 
   const now = new Date();
   const secondsSinceEpoch = Math.round(now.getTime() / 1000);
 
-  const tags = await tag.find({ name: { $in: data } });
+  const tags = await tag.find({ name: { $in: data.tags } });
 
   const availableTags = tags.map(t => t.name.toLowerCase());
-  const tagsToCreate = data.filter(d => availableTags.indexOf(d.toLowerCase()) < 0);
+  const tagsToCreate = data.tags.filter(d => availableTags.indexOf(d.toLowerCase()) < 0);
+  const tagsToUpdate = data.tags.filter(d => availableTags.indexOf(d.toLowerCase()) >= 0);
+
   if (tagsToCreate && tagsToCreate.length > 0) {
     const creatingTags = tagsToCreate.map(t => ({
       name: t,
@@ -20,5 +23,9 @@ exports.tagProcess = async job => {
     }));
     console.log('creating tags', creatingTags);
     await tag.insertMany(creatingTags);
+  }
+
+  if (tagsToUpdate && tagsToUpdate.length > 0) {
+    await tagTrend.tagTrendInc({ tags: tagsToUpdate, userId: data.userId });
   }
 };
